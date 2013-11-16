2dbe21a7baa195ada0178ec7b21b6b84fb0ef12e
Christoph Zamaitat <czamaitat@gmail.com>
2013-11-16 22:06:55 +0100

    implementation of FCGame methods

--- FlashCard/FCGame.m
+++ FlashCard/FCGame.m
@@ -12,27 +12,46 @@
 
 - (id)initWithcards:(NSMutableArray *)c
 {
+    if (self == [super init])
+    {
+        self.cards = c;
+        self.currentCard = 0;
+    }
     
+    return self;
 }
 
 - (BOOL)hasMoreCards
 {
-    
+    return self.currentCard < [self.cards count];
 }
 
 - (FCCard *)getNextCard
 {
-    
+    FCCard *card = [self.cards objectAtIndex:self.currentCard];
+    self.currentCard++;
+    return card;
 }
 
 - (int)getNumRight
 {
+    int numRight = 0;
     
+    for (int i = 0; i < [self.cards count]; ++i)
+    {
+        FCCard *c = [self.cards objectAtIndex:i];
+        if (c.isCorrect)
+        {
+            numRight++;
+        }
+    }
+    
+    return numRight;
 }
 
 - (int)getNumWrong
 {
-    
+    return [self.cards count] - [self getNumRight];
 }
 
 @end
